abd98c11084b6f6b8444367ad50bd5b5e43926a3
AfoDy <afodydigitals@gmail.com>
2023-07-31 19:12:47 -0700

    create subtraction function for the calculator

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //create variables to accept first and second user inputs
+let a = parseInt(prompt("Enter current year: "));
+let b = parseInt(prompt("Enter your year of birth: "));
 //create a subtraction function
+function subtraction() {
+    
+}
 //the subtraction function will return the result of the subtraction of the two inputs
 //Invoke subtraction function to run the calculator
