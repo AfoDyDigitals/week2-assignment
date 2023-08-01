0b608059d1e294d93228464f78b7440745e67209
AfoDy <afodydigitals@gmail.com>
2023-08-01 09:14:53 -0700

    create variables with the array

--- script.js
+++ script.js
@@ -12,7 +12,12 @@ console.log(subtraction());
 
 
 
-//Loop section
-let input = 30
-for (let i = 1; i<=input; i++);
-console.log(i);
+//Create an array of numbers
+let array = [1, 3, 4, 8, 11, 12, 15, 16, 17, 20]
+//
+let input = array
+for (let i=1; i<=input.length; i++) {
+    if(i%2 === 0){
+        console.log(i);
+    }
+}
