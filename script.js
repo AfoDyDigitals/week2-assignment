26acaedbff61d460e985d4a041a95525975ea9eb
AfoDy <afodydigitals@gmail.com>
2023-07-31 19:16:49 -0700

    create subtraction to be returned in the function

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let a = parseInt(prompt("Enter current year: "));
 let b = parseInt(prompt("Enter your year of birth: "));
 //create a subtraction function
 function subtraction() {
-    
-}
 //the subtraction function will return the result of the subtraction of the two inputs
+return a - b;
+}
+
 //Invoke subtraction function to run the calculator
